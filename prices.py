4d4453011c06e58ef51fa51420c3e79c1c464da0
Norik Hovhannisyan <hnorik453@gmail.com>
2026-03-22 16:29:20 +0400

    UI improvements with columns and sidebar

--- prices.py
+++ prices.py
@@ -14,28 +14,32 @@ st.write('Fill in the data to see the XGBoost model prediction.')
 
 ['Beds', 'Baths', 'Living Space', 'Zip Code Population', 'Zip Code Density', 'Median Household Income', 'Latitude', 'Longitude', 'Space_per_Room']
 
-# Beds                            3.065737
-# Baths                           2.299989
-# Living Space                 1743.306277
-# Zip Code Population         37997.154100
-# Zip Code Density             2223.449918
-# Median Household Income    104850.779437
-# Latitude                       36.482975
-# Longitude                     -97.296991
-# Space_per_Room                323.007613
+col1, col2 = st.columns(2)
 
-beds = st.number_input('Beds', value = 3)
-baths = st.number_input('Baths', value = 2)
-sqft = st.number_input('Living Space', value = 1740)
-pop = st.number_input('Zip Code Population', value = 38000)
-density = st.number_input('Zip Code Density', value = 2200)
-income = st.number_input('Median Household Income', value = 104000)
-lat = st.number_input('Latitude', value = 36)
-lon = st.number_input('Longitude', value = -97)
-room = st.number_input('Space_per_Room', value = 320)
+with col1:
+    beds = st.number_input('🛏️Beds', value = 3)
+    sqft = st.number_input('📐Living Space', value = 1740)
+    pop = st.number_input('👥Zip Code Population', value = 38000)
+    
+with col2:
+    baths = st.number_input('🚿Baths', value = 2)
+    income = st.number_input('💰Median Household Income', value = 104000)
+    density = st.number_input('🏙️Zip Code Density', value = 2200)
+    room = st.number_input('Space_per_Room', value = 320)
 
-if st.button('Calculate price'):
+st.sidebar.header('📍 Location Details')
+lat = st.sidebar.slider('Latitude', 24.0, 50.0, 36.48)
+lon = st.sidebar.slider('Longitude', -125.0, -66.0, -97.29)
+
+
+if st.button('🚀 Predict Market Price'):
     query = pd.DataFrame([[beds, baths, sqft, pop, density, income, lat, lon, room]], columns = features)
     prediction = model.predict(query)[0]
+    st.markdown('----')
+    st.subheader('Estimation Result')
+    st.metric(label="Predicted House Value", value=f"${prediction:,.0f}")
     st.success(f'Approximate price of the house: ${prediction:,.2f}')
-    
+    if prediction > 500000:
+        st.warning('This is considered a Luxury Property. 💎')
+    else:
+        st.info('This property is within the average market range. ✅')
